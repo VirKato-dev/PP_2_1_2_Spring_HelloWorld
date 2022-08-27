11b66263f0dc3f827bdc2beb8cd500a6b038bf6c
VirKato <dascat79@mail.ru>
2022-08-27 08:40:39 +0700

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1 == beanCat2);
     }
 }
